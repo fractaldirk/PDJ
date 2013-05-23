502784a4f7c1deb440da36fd7d33f590db871f83
fractaldirk <dirkgrandjean@live.nl>
2013-05-23 21:57:48 +0200

    without weekly updaters

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
   def statistics
     @top_users = User.find(:all, :order => "score DESC", :limit => 5)
-    @last_top_users = Song.find(:all, :conditions => {"created_at > ?", 1.week.ago}, :order => "points ASC", :group => "user_name", :limit => 5)
+    @last_top_users = Song.find(:all, :order => "points ASC", :group => "user_name", :limit => 5)
+    #need to include 1 week ago parameter!
   end
 
   def top_users
